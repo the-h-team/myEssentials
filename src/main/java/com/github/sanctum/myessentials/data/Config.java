468ced90d25b0d4d3b6611d92b6eb94128972cc1
Matthew Sanders <msanders5984@gmail.com>
2021-03-21 22:11:57 -0400

    Factory TLC; javadoc love

--- src/main/java/com/github/sanctum/myessentials/data/Config.java
+++ src/main/java/com/github/sanctum/myessentials/data/Config.java
@@ -1,29 +1,28 @@
 package com.github.sanctum.myessentials.data;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Encapsulates config file operations.
  */
 public class Config {
+    private static final Set<Config> INSTANCES = new HashSet<>();
     protected final String n;
     protected final String d;
     protected final File file;
     protected FileConfiguration fc;
-    private static final List<Config> configs = new ArrayList<>();
 
-    public Config(@NotNull final String n, final String d) {
+    private Config(@NotNull final String n, @Nullable final String d) {
         this.n = n;
         this.d = d;
         // Get the data directory of the plugin that is providing this Config implementation
@@ -35,9 +34,20 @@ public class Config {
             parent.mkdir();
         }
         this.file = new File(parent, n.concat(".yml"));
-        configs.add(this);
+        INSTANCES.add(this);
     }
 
+    /**
+     * Copy an InputStream directly to a given File.
+     * <p>
+     * Useful for placing resources retrieved from a JavaPlugin
+     * implementation at custom locations.
+     *
+     * @param in an InputStream, likely a plugin resource
+     * @param file the desire file
+     * @throws IllegalArgumentException if the file describes a directory
+     * @throws IllegalStateException if write is unsuccessful
+     */
     public static void copy(InputStream in, File file) {
         try {
             OutputStream out = new FileOutputStream(file);
@@ -48,39 +58,62 @@ public class Config {
             }
             out.close();
             in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("File is a directory!", e);
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to write to file! See log:", e);
         }
     }
 
+    /**
+     * Get the name of this Config.
+     *
+     * @return name of Config
+     */
     public String getName() {
-        return (n == null) ? "" : n;
+        return n;
     }
 
-    public String getDescription() {
-        return (d == null) ? "" : d;
+    /**
+     * Get the description of the config if it has one.
+     * <p>
+     * Used to resolve subdirectory if present.
+     *
+     * @return an Optional describing this config's description field
+     */
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(d);
     }
 
-    public static Config get(final String n, final String d) throws IllegalArgumentException {
-        for (final Config c : Config.configs) {
-            if (c.getName().equals(n) && c.getDescription().equals(d)) {
-                return c;
-            }
-        }
-        if (n == null || n.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be null/empty!");
-        }
-        return new Config(n, d);
-    }
-
+    /**
+     * Delete the backing file of this configuration.
+     * <p>
+     * Does not destroy backing YamlConfiguration; perform
+     * {@link Config#reload()} if that is desired as well.
+     *
+     * @return true if file was successfully deleted
+     */
     public boolean delete() {
         return file.delete();
     }
 
+    /**
+     * Check if the backing file currently exists.
+     * <p>
+     * Does interact whatsoever with the YamlConfiguration.
+     *
+     * @return true if file exists
+     */
     public boolean exists() {
         return file.exists();
     }
 
+    /**
+     * Get the backing file for this Config; additionally creates
+     * if not found.
+     *
+     * @return backing file File object
+     */
     public File getFile() {
         if(!file.exists()) {
             try {
@@ -93,7 +126,12 @@ public class Config {
         return file;
     }
 
-    public FileConfiguration getConfig() {
+    /**
+     * Get the FileConfiguration managed by this Config object.
+     *
+     * @return a File (Yaml) Configuration object
+     */
+    synchronized public FileConfiguration getConfig() {
         if(this.fc == null) {
             // fast exit with new blank configuration in the case of nonexistent file
             if (!file.exists()) return new YamlConfiguration();
@@ -103,19 +141,58 @@ public class Config {
         return this.fc;
     }
 
-    public void reload() {
+    /**
+     * Reload the file from disk.
+     * <p>
+     * If the backing file has been deleted, this method assigns a fresh,
+     * blank configuration internally to this object. Otherwise, the file
+     * is read from, directly replacing the existing configuration with
+     * its values. No attempt is made to save the existing configuration
+     * state, so keep that in mind when running this call.
+     */
+    synchronized public void reload() {
         if (!this.file.exists()) {
             this.fc = new YamlConfiguration();
         }
         this.fc = YamlConfiguration.loadConfiguration(this.file);
     }
 
-    public void saveConfig() {
+    /**
+     * Save the configuration to its backing file.
+     *
+     * @throws IllegalStateException if an error is encountered while saving
+     */
+    synchronized public void saveConfig() {
         try {
             getConfig().save(file);
         } catch (final IOException e) {
             throw new IllegalStateException("Unable to save configuration file!", e);
         }
+    }
+
+    /**
+     * Retrieve a Config instance via its name and description.
+     * <p>
+     * This method only resolves config objects for myEssentials; if you
+     * wish to manage your own configs see this method and the fields
+     * above for a general format of a static backing.
+     *
+     * @param name Name of config file
+     * @param desc Description of config file (designate subdirectory)
+     * @return existing instance or create new Config
+     * @throws IllegalArgumentException if name is empty
+     */
+    public static Config get(@NotNull final String name, final String desc) throws IllegalArgumentException {
+        // move up to fail fast
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty!");
+        }
+        // switch to stream api
+        final int hashCode = Objects.hash(name, desc);
+        return INSTANCES.stream()
+                .filter(c -> c.hashCode() == hashCode)
+                .findAny()
+                .orElseGet(() -> new Config(name, desc));
     }
 
     @Override
@@ -126,7 +203,7 @@ public class Config {
         return n.equals(config.n) &&
                 Objects.equals(d, config.d) &&
                 Objects.equals(fc, config.fc) &&
-                Objects.equals(getFile(), config.getFile());
+                Objects.equals(file, config.file);
     }
 
     @Override
