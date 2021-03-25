c4aeb37854ce4b9710ffcb6dafb26c8d5f9f5b4a
Matthew Sanders <msanders5984@gmail.com>
2021-03-25 01:46:23 -0400

    Add attribution header (license listed as TBD, but afaik must be GPL); exclude attribution from extended components to allow for non-free linking, prepare for separated modules

--- src/main/java/com/github/sanctum/myessentials/util/SignUtil.java
+++ src/main/java/com/github/sanctum/myessentials/util/SignUtil.java
@@ -1,3 +1,13 @@
+/*
+ *  Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *  This file is part of myEssentials, a derivative work inspired by the
+ *  Essentials <http://ess3.net/> and EssentialsX <https://essentialsx.net/>
+ *  projects, both licensed under the GPLv3.
+ *
+ *  This software is currently in development and its licensing has not
+ *  yet been chosen.
+ */
 package com.github.sanctum.myessentials.util;
 
 import org.bukkit.block.Block;
