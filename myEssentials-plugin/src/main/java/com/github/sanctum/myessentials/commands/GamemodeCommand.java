e1914d4c37a5b13ce04335b63fab9f9a6bc26860
Matthew Sanders <msanders5984@gmail.com>
2021-03-29 04:56:37 -0400

    Finish GamemodeCommand; (should we alias /gm to this or /gmc?)

--- myEssentials-plugin/src/main/java/com/github/sanctum/myessentials/commands/GamemodeCommand.java
+++ myEssentials-plugin/src/main/java/com/github/sanctum/myessentials/commands/GamemodeCommand.java
@@ -12,7 +12,13 @@ package com.github.sanctum.myessentials.commands;
 
 import com.github.sanctum.myessentials.model.CommandBuilder;
 import com.github.sanctum.myessentials.model.InternalCommandData;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -26,16 +32,67 @@ public final class GamemodeCommand extends CommandBuilder {
 	@Override
 	public @Nullable
 	List<String> tabComplete(@NotNull Player player, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
-		return null;
+		if (args.length < 2) {
+			// null = returns default completion (with player names)
+			return null;
+		} else if (args.length > 2) {
+			// return no entries
+			return Collections.emptyList();
+		}
+		return Arrays.asList("survival", "creative", "adventure", "spectator");
 	}
 
 	@Override
 	public boolean playerView(@NotNull Player player, @NotNull String commandLabel, @NotNull String[] args) {
-		return false;
+		if (args.length != 2) {
+			// message usage
+			sendUsage(player);
+			return false;
+		}
+		return setGamemode(player, args[1], args[0]);
 	}
 
 	@Override
 	public boolean consoleView(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+		if (args.length != 2) {
+			sendUsage(sender);
+			return false;
+		}
+		return setGamemode(sender, args[1], args[0]);
+	}
+
+	private boolean setGamemode(CommandSender sender, String gamemodeName, String playerName) {
+		final GameMode gameMode;
+		switch (gamemodeName.toLowerCase()) {
+			case "survival":
+				gameMode = GameMode.SURVIVAL;
+				break;
+			case "creative":
+				gameMode = GameMode.CREATIVE;
+				break;
+			case "adventure":
+				gameMode = GameMode.ADVENTURE;
+				break;
+			case "spectator":
+				gameMode = GameMode.SPECTATOR;
+				break;
+			default:
+				// invalid gamemode
+				return false;
+		}
+		final Player player = Bukkit.getPlayer(playerName);
+		if (player == null) {
+			// Name isn't a player
+			sendMessage(sender, "&cThat is not a valid player.");
+		} else if (!player.isOnline()) {
+			// Player must be online
+			sendMessage(sender, "&cThe player must be online.");
+		} else {
+			// We have a valid player
+			// valid player
+			player.setGameMode(gameMode);
+			sendMessage(sender, "&6Set &e" + playerName + " &6to " + gamemodeName + ".");
+		}
 		return false;
 	}
 }
