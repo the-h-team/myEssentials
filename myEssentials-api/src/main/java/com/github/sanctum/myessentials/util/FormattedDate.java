ad871cfe39a76483d6ccb8dcc7c6407c52079f05
Hempfest <64885225+Hempfest@users.noreply.github.com>
2021-03-31 10:21:13 -0700

    * Relocated FormattedDate api

--- myEssentials-api/src/main/java/com/github/sanctum/myessentials/util/FormattedDate.java
+++ myEssentials-api/src/main/java/com/github/sanctum/myessentials/util/FormattedDate.java
@@ -1,7 +1,6 @@
 package com.github.sanctum.myessentials.util;
 
 import com.github.sanctum.labyrinth.library.Message;
-import com.github.sanctum.myessentials.Essentials;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -11,7 +10,7 @@ import java.util.Locale;
 import java.util.Optional;
 import java.util.TimeZone;
 
-public class DateUtil {
+public class FormattedDate {
 
 	/**
 	 * Get a date object from the format (MM/DD/YYYY)
@@ -26,7 +25,7 @@ public class DateUtil {
 			DateFormat format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 			date = format.parse(time);
 		} catch (ParseException e) {
-			Message.loggedFor(Essentials.getInstance()).error("Unable to format date w/ format " + '"' + time + '"');
+			new Message("[myEssentials v1.0.0]").error("Unable to format date w/ format " + '"' + time + '"');
 		}
 		return Optional.ofNullable(date);
 	}
@@ -44,11 +43,11 @@ public class DateUtil {
 			String example = "{CURRENT_MONTH},{CURRENT_YEAR},2,12,30";
 			long month = Calendar.getInstance(TimeZone.getDefault()).get(Calendar.MONTH);
 			long year = Calendar.getInstance(TimeZone.getDefault()).get(Calendar.YEAR);
-			DateFormat format = new SimpleDateFormat("MM,yyyy,d,H,m", Locale.ENGLISH);
+			DateFormat format = new SimpleDateFormat("M,yyyy,d,H,m", Locale.ENGLISH);
 			String to = month + "," + year + "," + time;
 			date = format.parse(to);
 		} catch (ParseException e) {
-			Message.loggedFor(Essentials.getInstance()).error("Unable to format date w/ format " + '"' + time + '"');
+			new Message("[myEssentials v1.0.0]").error("Unable to format date w/ format " + '"' + time + '"');
 		}
 		return Optional.ofNullable(date);
 	}
