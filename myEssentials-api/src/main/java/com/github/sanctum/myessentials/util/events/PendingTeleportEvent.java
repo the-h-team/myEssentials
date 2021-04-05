876d73c5b6bd868752dccc76c75cdc0bf917e870
Matthew Sanders <msanders5984@gmail.com>
2021-04-04 21:29:29 -0400

    Add listening capacity to PendingTeleportEvent

--- myEssentials-api/src/main/java/com/github/sanctum/myessentials/util/events/PendingTeleportEvent.java
+++ myEssentials-api/src/main/java/com/github/sanctum/myessentials/util/events/PendingTeleportEvent.java
@@ -14,15 +14,14 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Encapsulates a requested teleportation.
- * <p>
- * This class cannot be directly listened to and serves
- * only as a base for pending teleport event types.
  */
 public abstract class PendingTeleportEvent extends Event implements Cancellable {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
     protected final Player player;
     protected final Destination destination;
     protected long delay;
@@ -99,5 +98,14 @@ public abstract class PendingTeleportEvent extends Event implements Cancellable 
     @Override
     public void setCancelled(boolean cancel) {
         this.cancelled = cancel;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
     }
 }
